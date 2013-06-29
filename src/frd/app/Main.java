809c4e5c4bf80b2a0a0a6723d688d30f02927b57
Sergio Viera <sergioviera@gmail.com>
2013-06-29 20:47:20 -0300

    a

--- src/frd/app/Main.java
+++ src/frd/app/Main.java
@@ -3,7 +3,6 @@ package frd.app;
 public class Main {
 	public static void main(String[] args){
 		System.out.println("*********** Iniciando ***********");
-
 		
 		System.out.println("*********** Fin de app ***********");
 	}
